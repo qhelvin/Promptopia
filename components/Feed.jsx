ab06e9ebb824ba27d9fdc7b276090b674dc8eb2e
Qhelvin <qhelvin@Qhelvins-MacBook-Air.local>
2023-05-08 12:08:34 +0200

    fix commit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,59 +1,77 @@
-'use client'
+"use client";
 
-import {useEffect,useState} from 'react'
+import { useEffect, useState } from "react";
 
-import PromptCard from './PromptCard'
+import PromptCard from "./PromptCard";
 
-const PromptCardList=({data, handleTagClick})=>{
-  return(
-    <div className='mt-16 prompt_layout'>
-        {data.map((post)=>(
-          <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-          />
-        ))}
-    </div>
-  )
-}
+const PromptCardList = ({ data }) => {
+	return (
+		<div className="mt-16 prompt_layout">
+			{data.map((post) => (
+				<PromptCard key={post._id} post={post} />
+			))}
+		</div>
+	);
+};
 
 const Feed = () => {
+	const [searchText, setSearchText] = useState("");
+	const [searchedResult, setSearchedResult] = useState([]);
+	const [posts, setPosts] = useState([]);
+	const [searchTimeOut, setSearchTimeOut] = useState(null);
 
-  const [searchText,setSearchText]=useState('')
-  const [posts,setPosts]=useState([])
-  
-  const handleSearchChange=(e)=>{
-    setSearchText(e.target.value)
-  }
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch("/api/prompt");
+			const data = await response.json();
 
-  useEffect(()=>{
-    const fetchPosts= async ()=>{
-      const response=await fetch('/api/prompt')
-      const data = await response.json()
+			setPosts(data);
+		};
 
-      setPosts(data)
-    }
+		fetchPosts();
+	}, []);
 
-    fetchPosts()
-  },[])
-  return (
-    <section className='feed'>
-      <form className='relative w-full flex-center'>
-        <input 
-        type="text"
-        placeholder='Search for a tag or username'
-        value={searchText}
-        onChange={handleSearchChange}
-        required
-        className='search_input peer' />
-      </form>
+	const filterPrompts = () => {
+		const regex = new RegExp(searchText, "i");
+		return posts.filter(
+			(item) =>
+				regex.test(item.creator.username) ||
+				regex.test(item.tag) ||
+				regex.test(item.prompt)
+		);
+	};
 
-      <PromptCardList
-      data={posts}
-      handleTagClick={()=>{}}/>
-    </section>
-  )
-}
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeOut);
+		setSearchText(e.target.value);
 
-export default Feed
+		setSearchTimeOut(
+			setTimeout(() => {
+				const searchResult = filterPrompts(e.target.value);
+				setSearchedResult(searchResult);
+			}, 500)
+		);
+	};
+
+	return (
+		<section className="feed">
+			<form className="relative w-full flex-center">
+				<input
+					type="text"
+					placeholder="Search for a tag or username"
+					value={searchText}
+					onChange={handleSearchChange}
+					required
+					className="search_input peer"
+				/>
+			</form>
+			{searchText ? (
+				<PromptCardList data={searchedResult} />
+			) : (
+				<PromptCardList data={posts} />
+			)}
+		</section>
+	);
+};
+
+export default Feed;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import {useSession} from 'next-auth/react'
 import {usePathname, useRouter} from 'next/navigation'
 
-const PromptCard = ({post, handleTagClick,handleEdit, handleDelete}) => {
+const PromptCard = ({post,handleEdit, handleDelete}) => {
   const {data : session}=useSession()
   const pathName=usePathname()
   const router= useRouter()
